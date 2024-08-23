3d4e6df5af5faca05fe669399f75694dbc2592f0
Andre Mansley <andremansley@gmail.com>
2024-08-23 23:06:04 +0100

    Basic Structure for playing cards grid and hover effects. Cards data request fixes

--- src/Components/CardGrid.jsx
+++ src/Components/CardGrid.jsx
@@ -3,14 +3,18 @@ import {useQuery} from "@tanstack/react-query";
 const CardGrid = () => {
 
     const getCards = () => {
-        fetch(`${import.meta.env.VITE_DATA_URL}/cards.json`, {})
-            .then(response => {
+        return fetch(`${import.meta.env.VITE_DATA_URL}/cards.json`, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((response) => {
                 if (!response.ok) {
                     throw new Error('Failed to fetch cards.');
                 }
                 return response.json();
-            })
-    }
+            });
+    };
 
     const {data: cardsData, isSuccess: isCardsDataSuccess} = useQuery({ queryKey: ['card_list'], queryFn: getCards })
     console.log(cardsData);
@@ -19,13 +23,15 @@ const CardGrid = () => {
         <div className="container mx-auto p-4">
             <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
                 {isCardsDataSuccess && cardsData?.map(card => (
-                    <div key={card.id} className="bg-white rounded-lg shadow-lg p-4">
+                    <div
+                        key={card.id}
+                        className="bg-white rounded-lg shadow-lg p-4 transform transition duration-300 hover:scale-105 hover:shadow-2xl hover:rotate-3 cursor-pointer"
+                    >
                         <img src={card.picture} alt={card.name} className="w-full rounded-md mb-4"/>
                         <p className="text-center font-semibold">{card.name}</p>
                     </div>
                 ))}
             </div>
-
         </div>
     );
 };
